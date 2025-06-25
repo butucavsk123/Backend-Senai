ea01b962f99d55375a25b9b58f52580e944cddb0
butucavsk123 <butucavsk@gmail.com>
2025-06-25 09:58:18 -0300

    Meu teste2

---
+++ Aula2.py
@@ -0,0 +1,8 @@
+Python 3.11.2 (main, Apr 28 2025, 14:11:48) [GCC 12.2.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> idade = 20
+>>> if idade < 18:
+...     print("Você é menor de idade.")
+... elif idade >= 18 and idade < 65:
+...     print("Você é adulto.")
+... else:
