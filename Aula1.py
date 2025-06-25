4d3cad74d1e882ae034ddaeb0234c88a85b3a6d6
butucavsk123 <butucavsk@gmail.com>
2025-06-24 23:48:50 -0300

    Testando

---
+++ Aula1.py
@@ -0,0 +1,6 @@
+Python 3.11.2 (main, Apr 28 2025, 14:11:48) [GCC 12.2.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 1
+>>> b = 3
+>>> print (a + b)
+4
